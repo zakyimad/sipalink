57f60af8595190e5a7aba2ad21c38d88519c6bee
zarex1110 <zakyimad@gmail.com>
2024-08-19 10:43:13 +0700

    Perbaikan Asset
    Perbaikan asset menjadi secure_asset

--- resources/views/home/layouts/main.blade.php
+++ resources/views/home/layouts/main.blade.php
@@ -18,15 +18,15 @@
 </body>
 
 <!-- Vendor JS Files -->
-<script src={{ asset("assets/vendor/aos/aos.js")}}></script>
-<script src={{ asset("assets/vendor/bootstrap/js/bootstrap.bundle.min.js")}}></script>
-<script src={{ asset("assets/vendor/glightbox/js/glightbox.min.js")}}></script>
-<script src={{ asset("assets/vendor/isotope-layout/isotope.pkgd.min.js")}}></script>
-<script src={{ asset("assets/vendor/swiper/swiper-bundle.min.js")}}></script>
-<script src={{ asset("assets/vendor/waypoints/noframework.waypoints.js")}}></script>
-<script src={{ asset("assets/vendor/php-email-form/validate.js")}}></script>
+<script src={{ secure_asset("assets/vendor/aos/aos.js")}}></script>
+<script src={{ secure_asset("assets/vendor/bootstrap/js/bootstrap.bundle.min.js")}}></script>
+<script src={{ secure_asset("assets/vendor/glightbox/js/glightbox.min.js")}}></script>
+<script src={{ secure_asset("assets/vendor/isotope-layout/isotope.pkgd.min.js")}}></script>
+<script src={{ secure_asset("assets/vendor/swiper/swiper-bundle.min.js")}}></script>
+<script src={{ secure_asset("assets/vendor/waypoints/noframework.waypoints.js")}}></script>
+<script src={{ secure_asset("assets/vendor/php-email-form/validate.js")}}></script>
 
 <!-- Template Main JS File -->
-<script src={{ asset("assets/js/main.js")}}></script>
+<script src={{ secure_asset("assets/js/main.js")}}></script>
 
 </html>
